f167a084ca03b567879d8bdb8411a288e24b9345
gabimin <gabimin2030@gmail.com>
2024-09-27 15:06:43 -0300

    web-data2

--- using_urllib.py
+++ using_urllib.py
@@ -1,10 +1,3 @@
-""" import urllib.request
-
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-for line in fhand:
-    print(line.decode().strip())
- """
-
 import urllib.request
 
 try:
